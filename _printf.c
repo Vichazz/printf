0e7078fe986b9becfe54f26daa83d7e370c8d3a2
Vichazz <oyeniyivt@gmail.com>
2023-01-24 15:19:49 -0800

    updated

--- _printf.c
+++ _printf.c
@@ -37,8 +37,7 @@ width = get_width(format, &i, list);
 precision = get_precision(format, &i, list);
 size = get_size(format, &i);
 ++i;
-printed = handle_print(format, &i, list, buffer,
-				 flags, width, precision, size);
+printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
 if (printed == -1)
 return (-1);
 printed_chars += printed;
