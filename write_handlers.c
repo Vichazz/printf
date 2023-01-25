97764d0bee80a3569988d74c4c99eeef2f150ae4
Ola-mide1 <olamidesuccess9@gmail.com>
2023-01-24 18:26:22 -0800

    team project

--- write_handlers.c
+++ write_handlers.c
@@ -13,7 +13,8 @@
  * Return: Number of chars printed.
  */
 
-int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size)
+int handle_write_char(char c, char buffer[],
+		int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
 int i = 0;
 char padd = ' ';
@@ -37,7 +38,8 @@ if (flags & F_MINUS)
 return (write(1, &buffer[0], 1) +
 write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 else
-return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) + write(1, &buffer[0], 1));
+return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1)
+		+ write(1, &buffer[0], 1));
 }
 
 return (write(1, &buffer[0], 1));
@@ -57,7 +59,8 @@ return (write(1, &buffer[0], 1));
  * Return: Number of chars printed.
  */
 
-int write_number(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size)
+int write_number(int is_negative, int ind, char buffer[],
+		int flags, int width, int precision, int size)
 {
 int length = BUFF_SIZE - ind - 1;
 char padd = ' ', extra_ch = 0;
@@ -73,7 +76,8 @@ extra_ch = '+';
 else if (flags & F_SPACE)
 extra_ch = ' ';
 
- return (write_num(ind, buffer, flags, width, precision, length, padd, extra_ch));
+return (write_num(ind, buffer, flags, width, precision,
+			length, padd, extra_ch));
 }
 
 /**
@@ -89,7 +93,8 @@ extra_ch = ' ';
  *
  * Return: Number of printed chars.
  */
-int write_num(int ind, char buffer[], int flags, int width, int prec, int length, char padd, char extra_c)
+int write_num(int ind, char buffer[], int flags,
+		int width, int prec, int length, char padd, char extra_c)
 {
 int i, padd_start = 1;
 
@@ -124,7 +129,8 @@ else if (!(flags & F_MINUS) && padd == '0')/* extra char to left of padd */
 {
 if (extra_c)
 buffer[--padd_start] = extra_c;
-return (write(1, &buffer[padd_start], i - padd_start) + write(1, &buffer[ind], length - (1 - padd_start)));
+return (write(1, &buffer[padd_start], i - padd_start)
+		+ write(1, &buffer[ind], length - (1 - padd_start)));
 }
 }
 if (extra_c)
@@ -205,7 +211,8 @@ return (write(1, &buffer[ind], length));
  * Return: Number of written chars.
  */
 
-int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start)
+int write_pointer(char buffer[], int ind, int length,
+		int width, int flags, char padd, char extra_c, int padd_start)
 {
 int i;
 if (width > length)
@@ -235,7 +242,8 @@ if (extra_c)
 buffer[--padd_start] = extra_c;
 buffer[1] = '0';
 buffer[2] = 'x';
- return (write(1, &buffer[padd_start], i - padd_start) + write(1, &buffer[ind], length - (1 - padd_start) - 2));
+return (write(1, &buffer[padd_start], i - padd_start)
+		+ write(1, &buffer[ind], length - (1 - padd_start) - 2));
 }
 }
 buffer[--ind] = 'x';
